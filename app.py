3429af7de4d158e54b794a468426a712edab8ab2
derOli101 <Oliver.Braun@outlook.de>
2025-04-08 02:58:36 +0200

    changed port to 80

--- app.py
+++ app.py
@@ -145,4 +145,4 @@ def logout():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Nur beim ersten Start nötig
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=80)
